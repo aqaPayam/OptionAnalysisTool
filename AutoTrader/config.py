5a71b272b3ddce83abacf2d0562d5d8f66977b75
payam <xpayamtaebix@gmail.com>
2025-03-09 08:33:18 +0330

    moz

--- AutoTrader/config.py
+++ AutoTrader/config.py
@@ -15,8 +15,8 @@ class BaseConfig:
                       'Chrome/131.0.0.0 Safari/537.36',
         # Empty User-Agent
         'Accept': 'application/json, text/plain, */*',
-        'Cookie': '',
-        'x-sessionId': '',
+        'Cookie': 'cookiesession1=678B2928EAE15F00DBDF9A511BE7AE3E; otauth-178-OMS3308bf96-84d2-465d-b813-b6270982d2c3=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJTZXNzaW9uSWQiOiIzMzA4YmY5Ni04NGQyLTQ2NWQtYjgxMy1iNjI3MDk4MmQyYzMiLCJVc2VySWQiOiIxMDA3OTUiLCJBcHBOYW1lIjoiT01TIiwiQnJva2VyQ29kZSI6IjE3OCIsIm5iZiI6MTc0MTQ5NjUxNSwiZXhwIjoxNzQxNTI1MzE1LCJpc3MiOiJPTVMiLCJhdWQiOiJPTVMifQ.601giMiTALdhwijStoQpbI-OzaEUQf6qzlgbXotSOC2vYDCR8-Ro2tMD01aw5sLxocFcTCkKTIqkSft2f1MyAw',
+        'x-sessionId': 'OMS3308bf96-84d2-465d-b813-b6270982d2c3',
         'Content-Type': 'application/json',
     }
 
